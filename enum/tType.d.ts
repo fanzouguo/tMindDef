2098707bba4828bfb0e404f3b69c14bdfa0ba2f6
上海深普软件.Co.David <leev@smpoo.com>
2021-11-12 07:02:44 +0800

    2021-11-12 07:02:43-

---
+++ enum/tType.d.ts
@@ -0,0 +1,35 @@
+declare namespace tmind {
+	/** JS类型字面量枚举
+	 */
+	 enum ETYPE {
+		arguments = 'arguments',
+		array = 'array',
+		arraybuffer = 'arraybuffer',
+		boolean = 'boolean',
+		date = 'date',
+		dataview = 'dataview',
+		error = 'error',
+		float32array = 'float32array',
+		float64array = 'float64array',
+		function = 'function',
+		int16array = 'int16array',
+		int32array = 'int32array',
+		int8array = 'int8array',
+		map = 'map',
+		number = 'number',
+		object = 'object',
+		regexp = 'regexp',
+		set = 'set',
+		string = 'string',
+		symbol = 'symbol',
+		uint16array = 'uint16array',
+		uint32array = 'uint32array',
+		uint8array = 'uint8array',
+		uint8clampedarray = 'uint8clampedarray',
+		weakmap = 'weakmap',
+		weakref = 'weakref',
+		weakset = 'weakset'
+	}
+}
+
+export {}
